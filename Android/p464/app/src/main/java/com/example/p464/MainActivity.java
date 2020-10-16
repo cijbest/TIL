c30c4b7a02104cc0df43f0d966139baff4bcb581
cijbest <cijbest@gmail.com>
2020-10-16 10:21:06 +0900

    Update Android(p464)

---
+++ Android/p464/app/src/main/java/com/example/p464/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.p464;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        // SeekBar에 움직임이 발생할 때의 이벤트
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(progress+"");
+                setBright(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    public void setBright(int value){
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.screenBrightness = (float)value/100;
+        getWindow().setAttributes(params);
+    }
+}
