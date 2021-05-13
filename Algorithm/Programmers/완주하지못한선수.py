c77e3ed0a1a6c1d6f965671cf381f3835b121e25
cijbest <cijbest@gmail.com>
2021-05-13 20:26:51 +0900

    Add Programmers

---
+++ Algorithm/Programmers/완주하지못한선수.py
@@ -0,0 +1,13 @@
+def solution(participant, completion):
+    
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+        
+    return participant[-1]
+
+
+    
